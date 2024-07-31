737bc8abebc1b174e900e6ad8419e0e8c16eaec9
zenny126 <ntdg126@gmail.com>
2024-07-31 19:07:28 +0700

    home
    HomeSevlet+ view

--- src/java/controller/HomeServlet.java
+++ src/java/controller/HomeServlet.java
@@ -1,32 +1,51 @@
 package controller;
 
-import model.Category;
-import model.Product;
 import dao.CategoryDao;
 import dao.ProductDao;
+import model.Category;
+import model.Product;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao = new ProductDao();
-        CategoryDao categoryDao = new CategoryDao();
+    private static final long serialVersionUID = 1L;
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    public void init() {
+        categoryDao = new CategoryDao();
+        productDao = new ProductDao();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Category> categories = null;
         try {
-            List<Product> products = productDao.getAllProducts();
-            List<Category> categories = categoryDao.getAllCategories();
-            request.setAttribute("products", products);
-            request.setAttribute("categories", categories);
-            request.getRequestDispatcher("home.jsp").forward(request, response);
-        } catch (SQLException e) {
-            e.printStackTrace();
+            categories = categoryDao.getAllCategories();
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+        List<Product> products = null;
+        try {
+            products = productDao.getAllProducts();
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        request.setAttribute("categories", categories);
+        request.setAttribute("products", products);
+
+        request.getRequestDispatcher("home.jsp").forward(request, response);
     }
 }
